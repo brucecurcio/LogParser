e83036ebe5acc750ab6d93753ec1e58dec4c87c4
brucecurcio <brucecurcio@gmail.com>
2018-03-14 00:20:18 -0700

    14MAR18

--- TSIG_auditLog_scrub.py
+++ TSIG_auditLog_scrub.py
@@ -12,47 +12,154 @@ import os
 import time
 import datetime
 import csv
+import re
+import glob
 
 
-def main():
-    in_file = open('audit.log.0', 'r')
-    auditFile = str(in_file.readlines())
+def NSGLogExtractor(logFile):
+
+    in_file = open(logFile, 'r')
+    auditFile = in_file.readlines()
     in_file.close()
 
-    extPriLoc = auditFile.find("external_primaries")
-    extSecLoc = auditFile.find("external_secondaries")
+    nsgModEntries = []
 
-    extPrim = auditFile[extPriLoc:extSecLoc]
-    beforeArrowPri = extPrim.find("->")
-    extPrimCurrent = extPrim[20:beforeArrowPri-1]
-    extPrimChange = extPrim[beforeArrowPri+3:-2]
-    extPrimCurrSet = set(extPrimCurrent.split("]"))
-    extPrimChgSet = set(extPrimChange.split("]"))
-    extPrimAdd = extPrimCurrSet.union(extPrimChgSet) - extPrimCurrSet
-    extPrimDelete = extPrimCurrSet.union(extPrimChgSet) - extPrimChgSet
+    for line in auditFile:
+        if 'Modified NsGroup' in line:
+            nsgModEntries.append(line)
 
-    extSec = auditFile[extSecLoc:]
-    beforeArrowSec = extSec.find("->")
-    extSecCurrent = extSec[22:beforeArrowSec-1]
-    extSecChange = extSec[beforeArrowSec+3:-5]
-    extSecCurrSet = set(extSecCurrent.split("]"))
-    extSecChgSet = set(extSecChange.split("]"))
+#    print(nsgModEntries[600])
+    return nsgModEntries
+
+def ExtractEntry(auditList):
+    entry = str(auditList)
+#    print(entry)
+
+    date = entry[0:24]
+#    print(date)
+
+    nameGrp = re.search('Modified NsGroup (.*?):', entry).group(0)
+#    print(nameGrp)
+
+    action = re.search('Changed (.*?):', entry).group(0)
+#    print(action)
+
+    fh = open("nsgChgFile.txt", "a")
+    fh.write(date + " | " + nameGrp + " | " + action + "\n")
+    fh.close()
+
+    print(date + " | " + nameGrp + " | " + action)
+
+def DeltaLocator(auditEntry):
+
+    entryString = str(auditEntry)
+
+    extPri = re.search('external_primaries:\[\[(.*?)external_secondaries', entryString).group(0)
+#    print (extPri)
+
+    extSec = re.search('external_secondaries(.*$)', entryString).group(0)
+#    print(extSec)
+
+    extPriCurr = re.search('name(.*?)->', extPri).group(0)
+    extPriChg = re.search('->(.*$)', extPri).group(0)
+    #print(extPriCurr)
+    #print(extPriChg)
+
+    extPriCurrList = re.findall('name=\"(.*?\".*?\".*?)\"', extPriCurr)
+    #    print(extPriCurrList)
+    extPriChgList = re.findall('name=\"(.*?\".*?\".*?)\"', extPriChg)
+    #    print(extPriChgList)
+
+    extPriCurrSet = set(extPriCurrList)
+    extPriChgSet = set(extPriChgList)
+
+    extPriAdd = extPriCurrSet.union(extPriChgSet) - extPriCurrSet
+    extPriDelete = extPriCurrSet.union(extPriChgSet) - extPriChgSet
+    print("Added Ext Primaries " + str(extPriAdd))
+    print("Deleted Ext Primaries " + str(extPriDelete))
+
+
+    extSecCurr = re.search('name(.*?)->', extSec).group(0)
+    extSecChg = re.search('->(.*$)', extSec).group(0)
+    #print(extSecCurr)
+    #print(extSecChg)
+
+
+
+    extSecCurrList = re.findall('name=\"(.*?\".*?\".*?)\"', extSecCurr)
+    extSecChgList = re.findall('name=\"(.*?\".*?\".*?)\"', extSecChg)
+
+    extSecCurrSet = set(extSecCurrList)
+    extSecChgSet = set(extSecChgList)
+
     extSecAdd = extSecCurrSet.union(extSecChgSet) - extSecCurrSet
     extSecDelete = extSecCurrSet.union(extSecChgSet) - extSecChgSet
+    print("Added Ext Secondaries " + str(extSecAdd))
+    print("Deleted Ext Secondaries " + str(extSecDelete))
 
-#    print ("wholefile " + auditFile)
+    fh = open("nsgChgFile.txt", "a")
+    if (extPriAdd != set()):
+        fh.write("Added Ext Primaries " + str(extPriAdd) + "\n")
+    if (extPriDelete != set()):
+        fh.write("Deleted Ext Primaries " + str(extPriDelete) + "\n")
+    if (extSecAdd != set()):
+        fh.write("Added Ext Secondaries " + str(extSecAdd) + "\n")
+    if (extSecDelete != set()):
+        fh.write("Deleted Ext Secondaries " + str(extSecDelete) + "\n")
+    fh.write("\n")
+    fh.close()
+
+    extSecCurrList = re.findall('name=\"(.*?\".*?\".*?)\"', extSecCurr)
+#    print(extSecCurrList)
+    extSecChgList = re.findall('name=\"(.*?\".*?\".*?)\"', extSecChg)
+#    print(extSecChgList)
+
+
+    # extPriLoc = auditEntry.find("external_primaries")
+#    extSecLoc = auditEntry.find("external_secondaries")
+
+#    extPrim = auditEntry[extPriLoc:extSecLoc]
+#    beforeArrowPri = extPrim.find("->")
+#    extPrimCurrent = extPrim[20:beforeArrowPri-1]
+#    extPrimChange = extPrim[beforeArrowPri+3:-2]
+#    extPrimCurrSet = set(extPrimCurrent.split("]"))
+#    extPrimChgSet = set(extPrimChange.split("]"))
+#    extPrimAdd = extPrimCurrSet.union(extPrimChgSet) - extPrimCurrSet
+#    extPrimDelete = extPrimCurrSet.union(extPrimChgSet) - extPrimChgSet
+
+#    extSec = auditEntry[extSecLoc:]
+#    beforeArrowSec = extSec.find("->")
+#    extSecCurrent = extSec[22:beforeArrowSec-1]
+#    extSecChange = extSec[beforeArrowSec+3:-5]
+#    extSecCurrSet = set(extSecCurrent.split("]"))
+#    extSecChgSet = set(extSecChange.split("]"))
+#    extSecAdd = extSecCurrSet.union(extSecChgSet) - extSecCurrSet
+#    extSecDelete = extSecCurrSet.union(extSecChgSet) - extSecChgSet
+
+#    print ("wholefile " + auditEntry)
 #    print("extPrimCurrent " + extPrimCurrent)
 #    print("extPrimChange " + extPrimChange)
 #    print("extSecCurrent " + extSecCurrent)
 #    print("extSecChange " + extSecChange)
 #    print(extPrimCurrSet)
 #    print(extPrimChgSet)
-#    print(extPrimAdd)
-#    print(extPrimDelete)
-    print(extSecCurrSet)
-    print(extSecChgSet)
-    print(extSecAdd)
-    print(extSecDelete)
+#    print("added Primaries "+str(extPrimAdd))
+#    print("deleted Primaries "+str(extPrimDelete))
+#    print(extSecCurrSet)
+#    print(extSecChgSet)
+#    print("added Secondaries "+str(extSecAdd))
+#    print("deleted Secondaries "+str(extSecDelete))
+    print("")
+
+def main():
+
+    os.chdir("./")
+    for file in glob.glob("audit*"):
+        print(file)
+        logEntries = NSGLogExtractor(file)
+        for line in logEntries:
+            ExtractEntry(line)
+            DeltaLocator(line)
 
 if __name__ == "__main__":
     main()
